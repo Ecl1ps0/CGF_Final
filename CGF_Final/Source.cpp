0182cb41aa13bdb60e9c735a8f15d3cf89d330aa
Ecl1ps0 <93914852+Ecl1ps0@users.noreply.github.com>
2023-11-03 00:42:29 +0600

    feat: init glfw window

--- CGF_Final/Source.cpp
+++ CGF_Final/Source.cpp
@@ -1,8 +1,41 @@
-#include <iostream>
+#include "Sphere.h"
+#include "Utils.h"
 
-using namespace std;
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <GL/freeglut.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <iostream>
+#include <vector>
 
 int main() {
-    cout << "Hello" << endl;
-    return 0;
+
+	if (!glfwInit()) {
+		std::cerr << "Failed to initialize GLFW" << std::endl;
+		return -1;
+	}
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Something", nullptr, nullptr);
+	if (!window) {
+		std::cerr << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
 }
